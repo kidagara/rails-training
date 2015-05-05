460890935be63710484c3bcc9b72e2323674016d
Josh Huntsman <joshhuntsman@Macintosh.local>
2015-05-05 11:03:58 -0600

    added username and removed email requirement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :name, :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password, :remember_me)}
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :name, :password, :password_confirmation, :current_password)}
+  end
 end
